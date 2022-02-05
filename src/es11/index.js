526b99952adeae0df870221302e8683c1724726d
yahir2021 <namucheyahir@gmail.com>
2022-02-05 13:05:59 -0500

    githud pages

--- src/es11/index.js
+++ src/es11/index.js
@@ -10,3 +10,30 @@ const anotherBigNumber = BigInt(9007199254740991);
 
 console.log(aBigNumber);
 console.log(anotherBigNumber);
+
+const promise1 = new Promise((resolve,reject) => reject("reject"));
+const promise2 = new Promise((resolve,reject) => resolve("resolve"));
+const promise3 = new Promise((resolve,reject) => resolve("resolve 1"));
+
+Promise.allSettled([promise1, promise2, promise3])
+    .then(response => console.log(response));
+
+
+console.log(window);
+console.log(globalThis);
+
+
+const fooo = null ?? 'default string';
+console.log(fooo);
+
+
+
+const user = {};
+console.log(user?.profile?.email);
+
+if(user?.profile?.email) {
+    console.log('email')
+} else {
+    console.log('fail');
+}
+ 
