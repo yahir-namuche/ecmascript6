2ebeb3b84adbba2217fbd7d5ff4103ba0461b61c
yahir-namuche <81106657+yahir-namuche@users.noreply.github.com>
2022-02-06 10:37:56 -0500

    es6

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,32 +1,42 @@
-function newFuction(name, age, country){
-    var name = name || 'Yahir';
-    var age = age || 12;
-    var country = country || 'PE'
-    console.log(name, age, country)
+const obj = {
+  name: 'oscar',
+  age: 32,
+  country: 'MX'
+};
+
+let { country, ...all } = obj;
+console.log(all);
+
+const obj = {
+  name: 'Oscar',
+  age: 32
 }
 
-//es6
-function newFunction2(name = 'Yahir', age = 12, country = "PE") {
-    console.log(name, age, country);
-  };
-  
-  newFunction2();
-  newFunction2('Luis', '9', 'PE');
-
-  let hello = "Hello";
-  let world = "World";
-  let epicPhrase = hello + ' ' + world;
-  console.log(epicPhrase);
-  let epicPhrase2 = `${hello} ${world}`;
-    console.log(epicPhrase2);
-
-{
-  var globalvar = "Globak var";
+const obj1 = {
+  ...obj,
+  country: 'MX'
 }
 
-{
-  let globalLet = 'Global Let';
-  console.log(globalLet);
-}
+console.log(obj1);
 
-console.log(globalvar);
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (true)
+      ? setTimeout(() => resolve('Hello World'), 3000)
+      : reject(new Error('Test Error'))
+  });
+};
+
+helloWorld()
+  .then(response => console.log(response))
+  .catch(error => console.log(error))
+  .finally(() => console.log('Finalizo'))
+
+
+  const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+  const match = regexData.exec('2018-04-20');
+  const year = match[1]
+  const month = match[2]
+  const day = match[3]
+
